d055fa9b454e8319bab770e518090ae76649a0b3
PIERRON Marc <pierronmarc@outlook.fr>
2022-03-21 02:08:42 +0100

    feat: add routing component

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,15 +1,1 @@
-<?php
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-/**
- * @var Request $request
- * @var Response $response
- */
-
-$name = $request->query->get('name', 'World');
-
-?>
-
-Hello <?= htmlspecialchars($name, ENT_QUOTES) ?>
+Hello <?= htmlspecialchars($name ?? 'World') ?>
