09871a3b07c5a455bb7fe65a02b65173217b8f6a
PIERRON Marc <pierronmarc@outlook.fr>
2022-03-24 20:43:15 +0100

    refactor: apply separation of concerns

--- src/app.php
+++ src/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Controller\GreetingsController;
-use App\Controller\LeapYearController;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -9,17 +7,7 @@ $routes = new RouteCollection();
 
 $routes->add('leap_year', new Route('/is_leap_year/{year}', [
     'year' => null,
-    '_controller' => [new LeapYearController(), 'index'],
-
-]));
-
-$routes->add('hello', new Route('/hello/{name}', [
-    'name' => 'World',
-    '_controller' => [new GreetingsController(), 'hello']
-]));
-
-$routes->add('bye', new Route('/bye', [
-    '_controller' => [new GreetingsController(), 'bye']
+    '_controller' => 'Calendar\Controller\LeapYearController::index',
 ]));
 
 return $routes;
