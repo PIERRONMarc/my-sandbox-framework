8cc22da36cff0ebf28816f40ee85b66ded09605b
PIERRON Marc <pierronmarc@outlook.fr>
2022-03-27 01:42:04 +0100

    feat: add httpKernel default implementation

--- src/Calendar/Controller/LeapYearController.php
+++ src/Calendar/Controller/LeapYearController.php
@@ -4,22 +4,16 @@ namespace Calendar\Controller;
 
 use Calendar\Model\LeapYear;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class LeapYearController
 {
-    public function index(Request $request, $year): Response
+    public function index(Request $request, $year)
     {
         $leapYear = new LeapYear();
         if ($leapYear->isLeapYear($year)) {
-            $response = new Response('Yep, this is a leap year!');
+            return 'Yep, this is a leap year!';
         } else {
-            $response = new Response('Nope, this is not a leap year.');
+            return 'Nope, this is not a leap year.';
         }
-
-        $response->setTtl(10);
-
-        return $response;
-
     }
 }
--- web/front.php
+++ web/front.php
@@ -2,13 +2,17 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Simplex\ContentLengthListener;
 use Simplex\Framework;
-use Simplex\GoogleListener;
+use Simplex\StringResponseListener;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\ErrorListener;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\EventListener\StreamedResponseListener;
 use Symfony\Component\HttpKernel\HttpCache\Esi;
 use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 use Symfony\Component\HttpKernel\HttpCache\Store;
@@ -16,19 +20,27 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
 $request = Request::createFromGlobals();
+$requestStack = new RequestStack();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new RequestContext();
 $matcher = new UrlMatcher($routes, $context);
 
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new ContentLengthListener());
-$dispatcher->addSubscriber(new GoogleListener());
-
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+$dispatcher->addSubscriber(new ResponseListener('UTF-8'));
+$dispatcher->addSubscriber(new StreamedResponseListener());
+$dispatcher->addSubscriber(new StringResponseListener());
+$listener = new ErrorListener(
+    'Calendar\Controller\ErrorController::exception'
+);
+$dispatcher->addSubscriber($listener);
+
+$framework = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+
 $framework = new HttpCache(
     $framework,
     new Store(__DIR__.'/../cache'),
